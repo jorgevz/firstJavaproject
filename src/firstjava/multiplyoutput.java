f6ea3d9bc3add9076e9876cdec644b9a9e417113
jorgevz <jorgev2132@gmail.com>
2020-08-25 19:35:10 -0400

    changed files

--- src/firstjava/multiplyoutput.java
+++ src/firstjava/multiplyoutput.java
@@ -1,31 +1,26 @@
 package firstjava;
 
+import java.util.Scanner;
+
 public class multiplyoutput {
- public static void main (String[] args) {
-	 
-	 /*the user here is able to put any number they choose to get the 
-	  * result for, however i inputed these numbers here just to 
-	  * show an example of where the numbers can be put at.
-	  */
-	 
-	 int userinput1  = 7 ;
-	 int userinput2 = 9 ;
-	 
-	 /*these are my conditions, the conditions are set so that no matter
-	  * what the user's input is, they will always get their answer like if using a calculator.
-	  */
-	 
-	 
-	 if (userinput1 <= userinput2 ) {
-	 System.out.println(userinput1 * userinput2);
-	 }  
-	 
-	 if (userinput2 <= userinput1) {
-		 System.out.print(userinput1 * userinput2);
-	 }
+	public static double outputs (double input1, double input2) {
+		double result = input1 * input2;
+		System.out.println(result);
+		return result; 
+	}
+	public static void main (String[] args) {
+		System.out.println("What's your first input ? ");
+		Scanner solution = new Scanner (System.in);
+		double input1 = solution.nextDouble();
+		System.out.println("What's your second input ? ");
+		double input2 = solution.nextDouble();
+		
+		double answer = outputs(input1, input2);
+		
+		System.out.println("Your result is: " + answer);
+		
 	
-	 
- }
+	}
 }
 
 
--- src/firstjava/unitcoversion.java
+++ src/firstjava/unitcoversion.java
@@ -1,5 +1,22 @@
 package firstjava;
 
-public class unitcoversion {
+import java.util.Scanner;
 
+public class unitcoversion {
+	 public static void conversion (double inch) {
+		 double meter = 0.0254;
+		 double solution = inch * meter;
+		 System.out.println(solution);
+	 }
+	public static void main (String[] args) {
+		System.out.println("How many inches would you like to convert? ");
+		Scanner convert  = new Scanner(System.in);
+		double inch = convert.nextDouble();
+		
+		conversion(inch); 
+		
+		
+		
+		
+}
 }
